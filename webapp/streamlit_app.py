164ec95886cec2c2b799159550f2c9c4949c62b3
inetke <ineta.keryte@gmail.com>
2026-02-25 22:50:24 +0000

    modified webapp streamlit

--- webapp/streamlit_app.py
+++ webapp/streamlit_app.py
@@ -128,7 +128,7 @@ if "week_start" not in df_base.columns:
 
 # Build absolute JSON path relative to this script
 base_dir = os.path.dirname(os.path.abspath(__file__))
-json_path = os.path.join(base_dir, "..", "models", "category_keywords.json")
+json_path = os.path.join(base_dir, "..", "src", "category_keywords.json")
 
 if not os.path.exists(json_path):
     st.error(f"❌ No encuentro el JSON en: {json_path}")
